455f6aa88b6a457e342b9228fd48e8a9e87504ee
Tom Nightingale <tom@weareleague.co.uk>
2021-05-25 12:25:28 +0100

    rework index.js

--- pages/[backup].js
+++ pages/[backup].js
@@ -2,6 +2,8 @@ import Head from 'next/head'
 import { request } from "../lib/datocms";
 import { metaTagsFragment, responsiveImageFragment } from "../lib/fragments";
 import Layout from '../components/layout'
+import Header from '../components/header'
+import Hero from '../components/hero'
 import { renderMetaTags } from "react-datocms";
 import { motion } from "framer-motion"
 import { fade } from "../lib/transitionHelpers"
@@ -17,6 +19,8 @@ export default function Home({ data: {home, site} }) {
             {renderMetaTags(home.seo.concat(site.faviconMetaTags))} 
         </Head>  
 
+        <Header navLinks={home.heroLinks} />
+
         <motion.div
           initial="initial"
           animate="enter"
@@ -24,16 +28,17 @@ export default function Home({ data: {home, site} }) {
         >  
          
           <motion.div variants={fade}>
-
-            <div className="flex flex-col items-center justify-center w-full min-h-screen text-white bg-primary">
-                    
-              <img className="mx-auto mb-8" src="images/adtrak-logo.svg" width={160} height={35} alt="Adtrak Media Limited" /> 
-              
-              <p>Please login to see our services.</p>
-
-              
-
-            </div>
+        
+            <Hero
+              index={true}
+              navLinks={home.heroLinks} 
+              heroTitle={home.heroTitle}
+              heroImage={home.heroImage}
+              heroBlurb={home.heroBlurb}
+              heroBgColor="bg-primary"
+              heroTextColor="text-white"
+              heroLinks={home.heroLinks}
+            />
 
           </motion.div>
 
@@ -52,6 +57,30 @@ const HOMEPAGE_QUERY = `
       }
     }
     home {
+      heroTitle
+      heroBlurb
+      heroImage {
+        responsiveImage(imgixParams: {fm: png, w:1920, h:796, crop: entropy, fit: crop }) {
+          ...responsiveImageFragment
+        }
+      }
+      heroLinks {
+        ... on HostingRecord {
+          pageTitle
+          heroTitle
+          slug
+        }
+        ... on InternetMarketingRecord {
+          pageTitle
+          heroTitle
+          slug
+        }
+        ... on WebDesignRecord {
+          pageTitle
+          heroTitle
+          slug
+        }
+      }
       seo: _seoMetaTags {
         ...metaTagsFragment
       }
