c06d54be261e974b030c8ad9b2e1ccf00c7d2aa9
Tom Nightingale <tom@weareleague.co.uk>
2022-03-02 17:08:04 +0000

    fix active state

--- components/header.js
+++ components/header.js
@@ -5,7 +5,8 @@ import Link from 'next/link';
 export default function header({ navLinks }) {
 
     const router = useRouter();
-    const currentLink = router.pathname.replace('/', '');
+    const currentLink = router.asPath.replace('/', '');
+    // console.log(router);
     
     return (
         <div className="absolute top-0 z-20 w-full pt-12">
@@ -25,7 +26,7 @@ export default function header({ navLinks }) {
                         <Link key={i} href={link.slug} scroll={false}>
                             <a 
                                 aria-label={`Go to ${link.pageTitle}`} 
-                                className={`inline-flex items-center p-2 mx-4 transition-all duration-500 group hover:underline ${currentLink !=="web-design" && currentLink === link.slug ? 'text-secondary-light' : 'text-white'} ${currentLink ==="web-design" && currentLink === link.slug ? 'text-secondary-dark' : 'text-white'} `}
+                                className={`inline-flex items-center p-2 mx-4 transition-all duration-500 group hover:underline ${currentLink === link.slug ? 'relative after:w-1 after:h-1 after:absolute after:top-0 after:left-1/2 after:rounded-full after:bg-white text-white' : 'text-white'} `}
                             >
                                 {link.pageTitle}
                             </a>
